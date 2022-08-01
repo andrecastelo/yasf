27771f44ef5d3d0d2d02c66fe29c1097e975783e
Andre Castelo <andrecastelo@gmail.com>
2022-08-01 09:56:54 +0200

    feat: add colors as css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { useState } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useMemo } from 'react';
 import { Scale } from './components/Scale';
+import { convertToVars } from './lib/utils';
+import { colors } from './theme';
+
+const getCssVariables = () => {
+  return convertToVars(colors);
+};
 
 export const App = () => {
+  const cssVars = useMemo(() => getCssVariables(), []);
+
   return (
-    <div className="App">
+    <div className="App" css={cssVars}>
       <Scale />
     </div>
   );
