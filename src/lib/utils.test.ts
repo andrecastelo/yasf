78a3585072d5f4c61c11b79c5d5ef2df80d7e17c
Andre Castelo <andrecastelo@gmail.com>
2022-08-01 09:27:19 +0200

    fix: issue where wrapAround would not work properly for all indexes

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,4 +1,5 @@
-import { test, expect, assert, describe } from 'vitest';
+import { test, expect, assert, describe, it } from 'vitest';
+import { SHARP_NOTES_ONLY } from './semitones';
 import { permutations, wrapAround } from './utils';
 
 test('lib/utils/permutations', () => {
@@ -19,6 +20,21 @@ test('lib/utils/permutations', () => {
 test('lib/utils/wrapAround', () => {
   const notes = 'cdefgab'.toUpperCase().split('');
 
+  it('has the same length', () => {
+    expect(wrapAround(notes, 0).length).toEqual(notes.length);
+    expect(wrapAround(notes, 1).length).toEqual(notes.length);
+    expect(wrapAround(notes, 2).length).toEqual(notes.length);
+    expect(wrapAround(notes, 3).length).toEqual(notes.length);
+    expect(wrapAround(notes, 4).length).toEqual(notes.length);
+    expect(wrapAround(notes, 5).length).toEqual(notes.length);
+    expect(wrapAround(notes, 6).length).toEqual(notes.length);
+  });
+
+  expect(wrapAround(notes, 0)).toEqual(['C', 'D', 'E', 'F', 'G', 'A', 'B']);
+  expect(wrapAround(notes, 1)).toEqual(['D', 'E', 'F', 'G', 'A', 'B', 'C']);
+  expect(wrapAround(notes, 2)).toEqual(['E', 'F', 'G', 'A', 'B', 'C', 'D']);
   expect(wrapAround(notes, 3)).toEqual(['F', 'G', 'A', 'B', 'C', 'D', 'E']);
-  expect(wrapAround(notes, 3).length).toEqual(notes.length);
+  expect(wrapAround(notes, 4)).toEqual(['G', 'A', 'B', 'C', 'D', 'E', 'F']);
+  expect(wrapAround(notes, 5)).toEqual(['A', 'B', 'C', 'D', 'E', 'F', 'G']);
+  expect(wrapAround(notes, 6)).toEqual(['B', 'C', 'D', 'E', 'F', 'G', 'A']);
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,5 +13,5 @@ export const permutations = <T>(array: T[]): T[][] => {
 export const wrapAround = <T>(array: T[], index: number) => {
   const slice = array.slice(index);
 
-  return slice.concat(array.slice(0, slice.length - 1));
+  return slice.concat(array.slice(0, array.length - slice.length));
 };
