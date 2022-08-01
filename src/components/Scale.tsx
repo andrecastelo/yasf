80e51a33c46841df02231a847c4edb3c5e46178f
Andre Castelo <andrecastelo@gmail.com>
2022-08-01 09:06:57 +0200

    chore: Scale component boilerplate

--- src/components/Scale.tsx
+++ src/components/Scale.tsx
@@ -1,3 +1,22 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const String = () => (
+  <div
+    css={css`
+      display: grid;
+      grid-template-columns: repeat(1fr, 12);
+      background-color: white;
+    `}
+  >
+    <p>hello</p>
+  </div>
+);
+
 export const Scale = () => {
-  return <div></div>;
+  return (
+    <div>
+      <String />
+    </div>
+  );
 };
