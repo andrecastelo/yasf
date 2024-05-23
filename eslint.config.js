e5e40100c82b68fb8845ef8c8a3a013194b7258b
Andre Castelo <andrecastelo@gmail.com>
2024-05-22 23:04:26 -0300

    chore: add react version to eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,23 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import recommendedReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    settings: {
+      react: {
+        version: '18',
+      },
+    },
     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    ...pluginReactConfig,
+    ...recommendedReactConfig,
     rules: {
-      ...pluginReactConfig.rules,
-      "react/no-unknown-property": ["error", { "ignore": ["css"] }],
+      ...recommendedReactConfig.rules,
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
       'react/react-in-jsx-scope': 'off',
     },
   },
